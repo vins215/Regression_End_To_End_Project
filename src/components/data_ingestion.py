852fde6e7faeaea90607e3553fdc3fa129e69ff4
vins215 <vinaycshekhar215@gmail.com>
2024-01-10 00:22:33 +0530

    IngestionAndTransformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,8 +5,8 @@ from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
 
-#from src.components.data_transformation import DataTransformation
 
 
 ## Intitialize the Data Ingetion Configuration
@@ -51,3 +51,5 @@ class DataIngestion:
 if __name__ =='__main__':
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr ,_=  data_transformation.initaite_data_transformation(train_data,test_data)
